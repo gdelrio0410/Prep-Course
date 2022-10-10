05007d6416204c1c1a5e9dd22532a20941e4ae3f
guillermo <gdelrio0410@gmail.com>
2022-10-09 19:36:48 -0500

    se le asigno un valor numerico a la variable nuevoNum

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,7 +4,7 @@
 const nuevaString = "soy un string";
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = null;
+const nuevoNum = 58;
 
 // Crea una variable booleana:
 const nuevoBool = null;
